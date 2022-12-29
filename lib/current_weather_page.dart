242a3b8de0a1d50958eedd2383f0ead4e5d917d3
MasaMune-68 <masamunedazeze@gmail.com>
2022-12-29 19:26:43 +0900

    ローカル通知機能の実装

--- lib/current_weather_page.dart
+++ lib/current_weather_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_coding_test_skeleton/main.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter_coding_test_skeleton/models/weather.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class CurrentWeatherPage extends StatefulWidget {
   const CurrentWeatherPage({super.key});
@@ -15,12 +17,19 @@ class _CurrentWeatherPageState extends State<CurrentWeatherPage> {
   Weather? _weather;
   String? latitude;
   String? longitude;
+  String LevelText = '位置情報をONにすると表示されます';
+
+  void main() {
+    notify();
+    notificationText;
+    runApp(MyApp());
+  }
 
   Widget build(BuildContext context) {
     return Scaffold(
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            getCurrentWeather();
+            notify();
           },
           child: const Icon(Icons.autorenew),
         ),
@@ -51,6 +60,10 @@ class _CurrentWeatherPageState extends State<CurrentWeatherPage> {
       Container(
         margin: const EdgeInsets.all(10.0),
         child: showLevelIcon(weather),
+      ),
+      Container(
+        margin: const EdgeInsets.all(10.0),
+        child: showLevelText(weather),
       ),
       Container(
         margin: const EdgeInsets.all(10.0),
@@ -106,7 +119,7 @@ class _CurrentWeatherPageState extends State<CurrentWeatherPage> {
     longitude = "$long";
 
     var url =
-        "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=hoge&units=metric";
+        "https://api.openweathermap.org/data/2.5/weather?lat=39.802768&lon=141.137075&appid=$apiKey&units=metric";
     final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
@@ -172,8 +185,47 @@ class _CurrentWeatherPageState extends State<CurrentWeatherPage> {
       return Image.asset('images/level4.png', scale: 15);
     } else if (weather.low > -8.0) {
       return Image.asset('images/level5.png', scale: 15);
-    } else {
-      print('error');
     }
+  }
+
+  showLevelText(Weather weather) {
+    if (weather.low > 0.0) {
+      return Text('水道管凍結の心配はないです');
+    } else if (weather.low > -3.0) {
+      return Text('水道管凍結の可能性があります');
+    } else if (weather.low > -5.0) {
+      return Text('水道管凍結に注意です');
+    } else if (weather.low > -7.0) {
+      return Text('水道管凍結に警戒です');
+    } else if (weather.low > -8.0) {
+      return Text('水道管の破裂に注意です');
+    }
+  }
+
+  notificationText(Weather weather) {
+    if (weather.low > 0.0) {
+      return LevelText = '水道管凍結の心配はないです';
+    } else if (weather.low > -3.0) {
+      return LevelText = '水道管凍結の可能性があります';
+    } else if (weather.low > -5.0) {
+      return LevelText = '水道管凍結に注意です';
+    } else if (weather.low > -7.0) {
+      return LevelText = '水道管凍結に警戒です';
+    } else if (weather.low > -8.0) {
+      return LevelText = '水道管の破裂に注意です';
+    }
+  }
+
+  //通知機能
+  Future<void> notify() {
+    final flnp = FlutterLocalNotificationsPlugin();
+    return flnp
+        .initialize(
+          InitializationSettings(
+            iOS: DarwinInitializationSettings(),
+          ),
+        )
+        .then((_) =>
+            flnp.show(0, '現在地の水道管凍結指数', LevelText, NotificationDetails()));
   }
 }
